bd4ea0680ec58169b203dc7f8c29592de5af4c28
ArtemDaemon <artem.daemon.official@gmail.com>
2024-06-21 18:52:57 +0700

    'https://' automatically adding if utl hasn't one

--- main.go
+++ main.go
@@ -5,22 +5,26 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const prefix string = "https://"
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		//b, err := io.ReadAll(resp.Body)
 		io.Copy(os.Stdout, resp.Body)
 		err = resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		//fmt.Printf("%s\n", b)
 	}
 }
