777425751b79a065f605f5e355e3be4345f04a8a
ArtemDaemon <artem.daemon.official@gmail.com>
2024-06-21 18:47:11 +0700

    Writing response body out

--- main.go
+++ main.go
@@ -14,12 +14,13 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
+		//b, err := io.ReadAll(resp.Body)
+		io.Copy(os.Stdout, resp.Body)
+		err = resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s\n", b)
+		//fmt.Printf("%s\n", b)
 	}
 }
